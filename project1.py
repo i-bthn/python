9b30f548001c9e58906a1d4a077ff1e317665d9c
Buthina Alansary <58885514+i-bthn@users.noreply.github.com>
2022-10-20 16:25:57 +0300

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,20 @@
+
+num = input(print("Enter number of phone:"))
+
+#valid num
+
+
+
+
+
+# database
+dict = {"1111111111": "Amal", "2222222222": "Mohammed", "3333333333": "Khadijah", "4444444444": "Abdullah",
+        "5555555555": "Rawan", "6666666666": "Faisal"}
+
+
+
+# Querey
+if  num in dict:
+    print(dict[num])
+else:
+    print("Sorry, the number is not found ")
