4aea072aab916ab48effedff6bf819ae2b52fa71
Buthina Alansary <58885514+i-bthn@users.noreply.github.com>
2022-10-27 17:16:49 +0300

    Update AdvancedSequence.py

--- AdvancedSequence.py
+++ AdvancedSequence.py
@@ -23,3 +23,21 @@ tuple =(1,2,3,4,5,6)
 print(text[0:10:3])
 print(list[1:5:3])
 print(tuple[0:5:2])
+
+    #serch index or slice
+    slice(2,3,4)
+    text.index("abc")
+    #num of item
+    len(text)
+    #Repet  item
+    text.count(s)
+    #serch boolean
+    if 'is' in text :
+    print('is' in text)
+    
+    s="Bthn"
+    print(s*4)
+    
+    
+    
+    
