971e99975669b96776ab5640cb634231122d9789
Buthina Alansary <58885514+i-bthn@users.noreply.github.com>
2022-10-28 21:49:42 +0300

    Update Advanced Strings.py

--- Advanced Strings.py
+++ Advanced Strings.py
@@ -3,3 +3,17 @@
 text ="This is python course"
 print(text.find('is'))
 print(text.rfind('is'))
+
+#split
+print(text.split())
+
+
+#valdation
+value1="3789792"
+value2="AMKCNCK"
+value3="3789EFL>."
+
+print(value1.isalnum())
+print(value2.isalpha())
+print(value1.isdigit())
+print(value1.isidentifier())
